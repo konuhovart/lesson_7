41ab77ba8e81318619d5b9022c40ecaeefec9ae3
Konuhov Artjom <122548295+konuhovart@users.noreply.github.com>
2023-02-19 21:06:57 +0100

    Add files via upload

---
+++ home_work_7/script/script.js
@@ -0,0 +1,20 @@
+const target = document.querySelector(".blue");
+target.addEventListener(`click`, function () {
+  alert(`#0000FF`);
+});
+const target2 = document.querySelector(".red");
+target2.addEventListener(`click`, function () {
+  alert(`#FF0000`);
+});
+const target3 = document.querySelector(".yellow");
+target3.addEventListener(`click`, function () {
+  alert(`#FFFF00`);
+});
+const target4 = document.querySelector(".green");
+target4.addEventListener(`click`, function () {
+  alert(`#00FF00`);
+});
+const target5 = document.querySelector(".black");
+target5.addEventListener(`click`, function () {
+  alert(`#000000`);
+});
